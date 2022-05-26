3880cc35d7701fc43b1fe15dc53ad43038e5c6c5
EasyGames1 <myu6ka@gmail.com>
2022-05-26 16:59:47 +0500

    first-commit

--- src/components/Autorization/Autorization.jsx
+++ src/components/Autorization/Autorization.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { client_id, redirect_uri, permissions, display } from '../../API/Service';
 
 const Autorization = () => {
-    const navigate = useNavigate();
-
     return (
-        <button
-            onClick={() => window.location.href = `https://oauth.vk.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${permissions}&display=${display}&revoke=1`}
-        >
-            Авторизоваться
-        </button>
+        <>
+            <a
+                href={`https://oauth.vk.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${permissions}&display=${display}&revoke=1`}
+            >
+                Авторизоваться
+            </a>
+        </>
     );
 };
 

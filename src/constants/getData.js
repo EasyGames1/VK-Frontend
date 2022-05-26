7484431f2b091deedd6654f4bb695aab99ca9cc2
EasyGames1 <myu6ka@gmail.com>
2022-05-27 00:22:38 +0500

    9

--- src/constants/getData.js
+++ src/constants/getData.js
@@ -1,6 +1,5 @@
-export const redirect_uri = 'http://localhost';
+export const redirect_uri = 'https://vk-frontend-ten.vercel.app';
 export const version = '5.81';
 export const client_id = '8176974';
 export const permissions = 'friends';
 export const display = 'popup';
-export const access_token = 'aad5afc232c3c3b5b337e7bb5a407ad6849673b6f159a58dccd2c26fb2f1ea1ddd4d7ea1359380f8963f3';
